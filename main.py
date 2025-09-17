511df2c7a64ae98493da5849c5c95cd3002e46b0
szymonw <handerdog3@gmail.com>
2025-09-17 19:43:30 +0200

    correlation matrix

--- main.py
+++ main.py
@@ -34,18 +34,8 @@ final_outcome_translator = {
 data['FinalOutcome'] = [final_outcome_translator[entry] for entry in data['FinalOutcome']]
 
 # print(data.tail(8))
-print(data['StudentLevel'].unique())
 
-## v. alpha is gonna exclude remaining categorial features - TaskType, Discipline and StudentLevel
-X = data.drop(columns=["UsedAgain", "SessionID", "SessionDate", "Discipline", "TaskType", "StudentLevel"])
-y = data["UsedAgain"].astype(int)  # True/False -> 1/0
-print( X.tail(10) )
-
-### looking at correlation of each feature
-
-# TODO
-
-# ##! percentage of students returning to AI depending on their SatisfactionRating - not sufficient
+# ##! percentage of students returning to AI depending on their SatisfactionRating and on day of session - not sufficient
 
 # used_again_pct = (
 #     data.groupby('SatisfactionRating')['UsedAgain']
@@ -53,19 +43,39 @@ print( X.tail(10) )
 # )
 # plt.plot(used_again_pct)
 
-# 
+used_again_pct = (
+    data.groupby('DaysSinceFirstSession')['UsedAgain']
+      .mean() * 100
+)
+#plt.plot(used_again_pct)
+ 
 
 # ##! many test plots
 
-sampled_data = data.sample(n=500, random_state=42)
+sampled_data = data.sample(n=5000, random_state=42)
 
 sns.lmplot(
     data=sampled_data,
-    x="AI_AssistanceLevel",
-    y="DaysSinceFirstSession",
+    x="SessionLengthMin",
+    y="TotalPrompts",
     col="StudentLevel",
       hue="UsedAgain",
     palette={True: "blue", False: "red"},
     fit_reg=False
 )
 plt.show()
+
+## v. alpha is gonna exclude remaining categorial features - TaskType, Discipline and StudentLevel
+X = data.drop(columns=["UsedAgain", "SessionID", "SessionDate", "Discipline", "TaskType", "StudentLevel"])
+y = data["UsedAgain"].astype(int)  # True/False -> 1/0
+print( X.tail(10) )
+
+### looking at correlation of each feature
+corr_data = X.assign(UsedAgain=y)
+corr = corr_data.corr(numeric_only=True)
+plt.figure(figsize=(10, 8))
+sns.heatmap(corr, annot=True, fmt=".2f", cmap="coolwarm", cbar=True)
+
+plt.title("Correlation Heatmap")
+plt.show()
+# TODO
