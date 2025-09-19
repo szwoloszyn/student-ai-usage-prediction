14c87c7b54c046ff1bcd4c18fac2f0065005423d
szymonw <handerdog3@gmail.com>
2025-09-19 19:17:04 +0200

    testing model

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-plt.hist(data[data['UsedAgain'] == True]['AI_AssistanceLevel'], 
-#        alpha=0.5, label='Used Again', color='green')
-# plt.hist(data[data['UsedAgain'] == False]['AI_AssistanceLevel'], 
-#        alpha=1, label='Used Again', color='red')
-# plt.legend()
